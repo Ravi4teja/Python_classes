76cb2b48602a8eb3a7a9a8691b9aa5139730a92b
Pannuru <ravi.tej.p@dxc.com>
2021-06-27 23:48:50 +0530

    1st and 2nd Classes

---
+++ identifiers.py
@@ -0,0 +1,23 @@
+
+
+# Identifier can have with alphanumeric characters but it should not start with number
+#we cant use the special characters like @$!&% except _
+#we cant use4 reserved characters True, False, and, if, else, 
+#Declare the identifier understandle
+
+name1 = "Ravi"
+name2 = 'Ravi'
+
+name3 = 'Krish\'s Pen'
+
+height = 5.8
+age = 12
+print(name3)
+print(height)
+print(age)
+
+
+
+# Hello I'm doing well
+
+print("Hello I\"m doing well")
---
+++ strings.py
@@ -0,0 +1,102 @@
+
+name = "Sreya"
+name2 = "Krish"
+
+print (name + name2)
+
+sentence = """Hi,Good morning!
+how are you.
+"""
+
+
+sentence = '''Hi, Good Evening!
+how are you.
+'''
+
+print(sentence)
+
+#find the number of characters in a string
+print(len(name))
+
+#capi
+print(name.upper())
+print(name2.lower())
+
+print("ravi teja is giving lession".title())
+print("ravi teja is giving lession".capitalize())
+
+print(type(name))
+
+num1 = 4
+
+print(type(num1))
+dec1 = 5.97
+
+print(type(dec1))
+
+print(dir(str))
+
+print(help(str.upper))
+
+print("Hi,  I am doing good. \nHow are you doing")
+print("Hi,  I am doing good.\tHow are you doing")
+print("Hi,  I am doing good.\bHow are you doing")
+
+
+print(name, name, name, name)
+# print(100*name)
+
+print(name[1])
+print(name2[0:3]) # print(name2[:3])
+print(name2[2:])
+
+print(name[2:4])
+
+quote = "Everyday is pleasant day"
+
+print(quote.find('pleasant'))
+print(quote.index('a'))
+print(quote.rindex('a'))
+
+print(quote.replace('pleasant', 'great'))
+
+print(quote)
+
+quote = quote.replace('pleasant', 'great')
+
+print(quote)
+
+message1 = "      Hello what are doing    "
+print(message1.strip())
+print(message1)
+message1 = message1.strip()
+print(message1)
+
+print(name.startswith('Sr'))
+print(name2.startswith('Sr'))
+
+print("hi how are you hi what are you doing".count('e'))
+
+print('Hello'.islower())
+
+numstring = "45263"
+
+print(numstring.isnumeric())
+
+numstring2 = "232raja"
+
+print(numstring2.isnumeric())
+
+
+greetings ="Hello"
+
+# print(greetings[7])
+
+somevar = "Hello have a great day"
+
+print(somevar.replace("great","Hi"))
+
+
+
+
+
